24c3c748f890120262934da2dc1104fa34843f2b
HDUY102 <leduy020702@gmail.com>
2024-11-19 17:05:20 +0700

    Create RPC Bullet

---
+++ BanSungOn/Source/BanSungOnl/Projectitle/Bullet.cpp
@@ -0,0 +1,57 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Bullet.h"
+
+#include "Net/UnrealNetwork.h"
+
+
+// Sets default values
+ABullet::ABullet()
+{
+	bReplicates = true;
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ABullet::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABullet::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if(HasAuthority())
+	{
+		FVector NewLocation = GetActorLocation() + Direction * BulletSpeed * DeltaTime;
+		SetActorLocation(NewLocation);
+	}
+}
+
+void ABullet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(ABullet, Direction);
+	DOREPLIFETIME(ABullet, BulletDmg);
+	DOREPLIFETIME(ABullet, BulletSpeed);
+}
+
+void ABullet::SetBulletProperties_Implementation(float Damage, float Speed)
+{
+	BulletDmg = Damage;
+	BulletSpeed = Speed;
+}
+
+void ABullet::SetDirectionBullet_Implementation(const FVector NewDirection)
+{
+	Direction = (NewDirection - GetActorLocation()).GetSafeNormal();
+	Direction *= BulletSpeed;
+	Direction.Z = 0.f;
+}
+
---
+++ BanSungOn/Source/BanSungOnl/Projectitle/Bullet.h
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Bullet.generated.h"
+
+UCLASS()
+class BANSUNGONL_API ABullet : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ABullet();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	
+	UPROPERTY(Replicated)
+	float BulletSpeed;
+	UPROPERTY(Replicated)
+	float BulletDmg;
+	UPROPERTY(Replicated)
+	FVector Direction;
+	UFUNCTION(Server, Unreliable)
+	void SetBulletProperties(float Damage, float Speed);
+	UFUNCTION(Server, Unreliable)
+	void SetDirectionBullet(const FVector NewDirection);
+};
