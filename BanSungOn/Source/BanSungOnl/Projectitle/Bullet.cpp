22a690b275f323aad4c3b13bdd1d1b99c451b89b
HDUY102 <leduy020702@gmail.com>
2024-11-25 22:45:00 +0700

    sure overlap on server

--- BanSungOn/Source/BanSungOnl/Projectitle/Bullet.cpp
+++ BanSungOn/Source/BanSungOnl/Projectitle/Bullet.cpp
@@ -54,13 +54,16 @@ void ABullet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeP
 void ABullet::OnOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
 	int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	AZombies* Zombies = Cast<AZombies>(OtherActor);
-	if(IsValid(Zombies))
+	if (HasAuthority())
 	{
-		if (BulletSpeed > 0.1f) // Check speed Bullet
+		AZombies* Zombies = Cast<AZombies>(OtherActor);
+		if(IsValid(Zombies))
 		{
-			Zombies->TakeDmg(BulletDmg);
-			Destroy();
+			if (BulletSpeed > 0.1f) // Check speed Bullet
+			{
+				Zombies->TakeDmg(BulletDmg);
+				Destroy();
+			}
 		}
 	}
 }
